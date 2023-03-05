612cbcf255e7bb42c141028ea0b42508db966a4d
12345prashant <113520215+12345prashant@users.noreply.github.com>
2023-03-05 20:54:03 +0530

    commited

---
+++ myportfolio.js
@@ -0,0 +1,5 @@
+const hireMeBtn = document.getElementById("hire-me-btn");
+
+hireMeBtn.addEventListener("click", function() {
+  window.open("https://12345PRASHANT.github.io/Portfoilio_prashant_tripathi.github.io/assets/RESUME.pdf", "_blank");
+});
